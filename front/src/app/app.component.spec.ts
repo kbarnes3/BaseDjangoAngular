b478fe6ca1582ab93fe822ac3035b5d7edc5ea9b
Kevin Barnes <kbarnes3@gmail.com>
2019-10-11 16:47:56 -0700

    Fixing tests

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('NewDjangoSite app is running!');
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('NewDjangoSite');
   });
 });
