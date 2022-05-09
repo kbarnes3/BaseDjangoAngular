590654e731a050e3f09b69dafd570fa4ec851346
Kevin Barnes <kbarnes3@gmail.com>
2022-05-09 15:05:18 -0700

    Switching to Python 3.10 locally

--- scripts/Update.ps1
+++ scripts/Update.ps1
@@ -9,12 +9,12 @@ $already_activated = . $PSScriptRoot\Ensure-Venv.ps1
 
 # Check Python version
 $venv_version = & python --version
-$installed_version = & py -3.6 --version
+$installed_version = & py -3.10 --version
 if ($venv_version -ne $installed_version) {
     Write-Status "Updating venv from $venv_version to $installed_version"
     deactivate
     $venv = Join-Path $project_root "venv"
-    & py -3.6 -m venv $venv --upgrade
+    & py -3.10 -m venv $venv --upgrade
     . $PSScriptRoot\Ensure-Venv.ps1 | Out-Null
 }
 
