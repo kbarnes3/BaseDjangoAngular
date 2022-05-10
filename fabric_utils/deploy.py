e648e86ff7dd79dcf3c95c7981ca63d629471eea
Kevin Barnes <kbarnes3@gmail.com>
2022-05-10 14:59:13 -0700

    Fixing new pylint errors

--- fabric_utils/deploy.py
+++ fabric_utils/deploy.py
@@ -48,23 +48,23 @@ def exists(conn: Connection, path: str) -> bool:
     return patchwork_exists(conn, path) # pylint: disable=E1120
 
 def get_repo_dir(config: str) -> str:
-    return '{0}/newdjangosite-{1}'.format(PYTHON_DIR, config)
+    return f'{PYTHON_DIR}/newdjangosite-{config}'
 
 
 def get_backend_dir(repo_dir: str) -> str:
-    return '{0}/back'.format(repo_dir)
+    return f'{repo_dir}/back'
 
 
 def get_frontend_dir(repo_dir: str) -> str:
-    return '{0}/front'.format(repo_dir)
+    return f'{repo_dir}/front'
 
 
 def get_virtualenv_python_bin(repo_dir: str) -> str:
-    return '{0}/venv/bin/python'.format(repo_dir)
+    return f'{repo_dir}/venv/bin/python'
 
 
 def get_secret_repo_dir(config: str) -> str:
-    return '{0}/newdjangosite-{1}-secrets'.format(PYTHON_DIR, config)
+    return f'{PYTHON_DIR}/newdjangosite-{config}-secrets'
 
 
 def get_secret_repo_name(config: str) -> str:
@@ -88,17 +88,16 @@ def deploy(conn, config, branch=None, secret_branch=None):
     secret_branch = get_secret_repo_branch(config, secret_branch)
     use_ssl = configuration['ssl']
 
-    print(Fore.GREEN + ('Deploying {0} from branch {1} with ' +
-                        'secret repo from branch {2}').format(
-                            config, branch, secret_branch))
+    print(Fore.GREEN + f'Deploying {config} from branch {branch} with ' +
+                        f'secret repo from branch {secret_branch}')
 
     repo_dir = get_repo_dir(config)
-    config_dir = '{0}/config/ubuntu-22.04'.format(repo_dir)
-    daily_scripts_dir = '{0}/cron.daily'.format(config_dir)
-    uwsgi_dir = '{0}/uwsgi'.format(config_dir)
-    nginx_dir = '{0}/nginx'.format(config_dir)
+    config_dir = f'{repo_dir}/config/ubuntu-22.04'
+    daily_scripts_dir = f'{repo_dir}/cron.daily'
+    uwsgi_dir = f'{config_dir}/uwsgi'
+    nginx_dir = f'{config_dir}/nginx'
     secret_repo_dir = get_secret_repo_dir(config)
-    ssl_dir = '{0}/{1}/ssl'.format(secret_repo_dir, config)
+    ssl_dir = f'{secret_repo_dir}/{config}/ssl'
 
     _update_source(conn, repo_dir, branch)
     _update_source(conn, secret_repo_dir, secret_branch)
@@ -111,24 +110,24 @@ def deploy(conn, config, branch=None, secret_branch=None):
     _reload_web(conn, config, nginx_dir, use_ssl, ssl_dir)
     _run_tests(conn, config, repo_dir)
 
-    print(Fore.GREEN + 'Deployment to {0} complete'.format(config))
+    print(Fore.GREEN + f'Deployment to {config} complete')
 
 
 def _update_source(conn: Connection, repo_dir: str, branch: str):
-    print(Fore.GREEN + 'update_source for {0}'.format(repo_dir))
+    print(Fore.GREEN + f'update_source for {repo_dir}')
     ensure_directory(conn, repo_dir, owning_group=WEBADMIN_GROUP, mod='ug+w')
     with conn.cd(repo_dir):
         conn.run('sudo git fetch origin')
-        conn.run('sudo git reset --hard origin/{0}'.format(branch))
+        conn.run(f'sudo git reset --hard origin/{branch}')
 
 
 def update_backend_dependencies(conn: Connection, repo_dir: str):
     print(Fore.GREEN + 'update_backend_dependencies')
 
-    venv_dir = '{0}/venv'.format(repo_dir)
+    venv_dir = f'{repo_dir}/venv'
     if not exists(conn, venv_dir):
         print(Fore.GREEN + 'Creating virtualenv')
-        conn.run('python3 -m venv --system-site-packages {0}'.format(venv_dir))
+        conn.run(f'python3 -m venv --system-site-packages {venv_dir}')
 
     with conn.cd(repo_dir):
         print(Fore.GREEN + 'Updating pip')
@@ -150,18 +149,18 @@ def _compile_source(conn: Connection,
 
     with conn.cd(backend_dir):
         conn.run('sudo find . -iname "*.pyc" -delete')
-        conn.run('sudo {0} -m compileall .'.format(python_bin))
-        conn.run('sudo {0} manage_{1}.py collectstatic --noinput'.format(python_bin, config))
+        conn.run(f'sudo {python_bin} -m compileall .')
+        conn.run(f'sudo {python_bin} manage_{config}.py collectstatic --noinput')
 
 
 def _update_scripts(conn: Connection, config: str, daily_scripts_dir: str):
     print(Fore.GREEN + 'update_scripts')
     cron_daily_dir = '/etc/cron.daily'
     with conn.cd(daily_scripts_dir):
-        conn.run('sudo cp newdjangosite-{0}-* {1}'.format(config, cron_daily_dir))
+        conn.run(f'sudo cp newdjangosite-{config}-* {cron_daily_dir}')
 
     with conn.cd(cron_daily_dir):
-        conn.run('sudo chmod 755 newdjangosite-{0}-*'.format(config))
+        conn.run(f'sudo chmod 755 newdjangosite-{config}-*')
 
 
 def _update_database(conn: Connection, config: str, repo_dir: str):
@@ -169,18 +168,18 @@ def _update_database(conn: Connection, config: str, repo_dir: str):
     backend_dir = get_backend_dir(repo_dir)
     python_bin = get_virtualenv_python_bin(repo_dir)
     with conn.cd(backend_dir):
-        conn.run('sudo {0} manage_{1}.py migrate'.format(python_bin, config))
+        conn.run(f'sudo {python_bin} manage_{config}.py migrate')
 
 
 def _reload_code(conn: Connection, config: str, uwsgi_dir: str):
     print(Fore.GREEN + 'reload_code')
     with conn.cd(uwsgi_dir):
-        conn.run('sudo cp newdjangosite-{0}.ini /etc/uwsgi/apps-available'.format(config))
-        conn.run('sudo chmod 644 /etc/uwsgi/apps-available/newdjangosite-{0}.ini'.format(config))
-        conn.run('sudo systemctl enable uwsgi-app@newdjangosite-{0}.socket'.format(config))
-        conn.run('sudo systemctl enable uwsgi-app@newdjangosite-{0}.service'.format(config))
-        conn.run('sudo systemctl start uwsgi-app@newdjangosite-{0}.socket'.format(config))
-        conn.run('sudo touch /var/run/uwsgi/newdjangosite-{0}.reload'.format(config))
+        conn.run(f'sudo cp newdjangosite-{config}.ini /etc/uwsgi/apps-available')
+        conn.run(f'sudo chmod 644 /etc/uwsgi/apps-available/newdjangosite-{config}.ini')
+        conn.run(f'sudo systemctl enable uwsgi-app@newdjangosite-{config}.socket')
+        conn.run(f'sudo systemctl enable uwsgi-app@newdjangosite-{config}.service')
+        conn.run(f'sudo systemctl start uwsgi-app@newdjangosite-{config}.socket')
+        conn.run(f'sudo touch /var/run/uwsgi/newdjangosite-{config}.reload')
 
 
 def _build_content(conn: Connection, repo_dir: str):
@@ -197,14 +196,14 @@ def _build_content(conn: Connection, repo_dir: str):
 def _reload_web(conn: Connection, config: str, nginx_dir: str, ssl: bool, ssl_dir: str):
     print(Fore.GREEN + 'reload_web')
     with conn.cd(nginx_dir):
-        conn.run('sudo cp {0}.yourdomain.tld /etc/nginx/sites-enabled/'.format(config))
+        conn.run(f'sudo cp {config}.yourdomain.tld /etc/nginx/sites-enabled/')
 
     if ssl:
         with conn.cd(ssl_dir):
-            conn.run('sudo cp {0}.yourdomain.tld.* /etc/nginx/ssl'.format(config))
-            conn.run('sudo chown root /etc/nginx/ssl/{0}.yourdomain.tld.*'.format(config))
-            conn.run('sudo chgrp root /etc/nginx/ssl/{0}.yourdomain.tld.*'.format(config))
-            conn.run('sudo chmod 644 /etc/nginx/ssl/{0}.yourdomain.tld.*'.format(config))
+            conn.run(f'sudo cp {config}.yourdomain.tld.* /etc/nginx/ssl')
+            conn.run(f'sudo chown root /etc/nginx/ssl/{config}.yourdomain.tld.*')
+            conn.run(f'sudo chgrp root /etc/nginx/ssl/{config}.yourdomain.tld.*')
+            conn.run(f'sudo chmod 644 /etc/nginx/ssl/{config}.yourdomain.tld.*')
 
     conn.sudo('/etc/init.d/nginx reload')
 
@@ -214,7 +213,7 @@ def _run_tests(conn: Connection, config: str, repo_dir: str):
     backend_dir = get_backend_dir(repo_dir)
     python_bin = get_virtualenv_python_bin(repo_dir)
     with conn.cd(backend_dir):
-        conn.run('{0} manage_{1}.py test'.format(python_bin, config))
+        conn.run(f'{python_bin} manage_{config}.py test')
 
 
 def checkout_branch(conn: Connection, repo_dir: str, config: str, branch: Optional[str] = None):
@@ -229,18 +228,18 @@ def checkout_branch(conn: Connection, repo_dir: str, config: str, branch: Option
 def deploy_global_config(conn, config):
     print(Fore.GREEN + 'deploy_global_config')
     repo_dir = get_repo_dir(config)
-    global_dir = '{0}/config/ubuntu-22.04/global'.format(repo_dir)
+    global_dir = f'{repo_dir}/config/ubuntu-22.04/global'
     nginx_conf = '/etc/nginx/nginx.conf'
     uwsgi_socket = '/etc/systemd/system/uwsgi-app@.socket'
     uwsgi_service = '/etc/systemd/system/uwsgi-app@.service'
 
-    conn.sudo('cp {0}/nginx.conf {1}'.format(global_dir, nginx_conf))
+    conn.sudo(f'cp {global_dir}/nginx.conf {nginx_conf}')
     set_permissions_file(conn, nginx_conf, 'root', 'root', '644')
 
-    conn.sudo('cp {0}/uwsgi-app@.socket {1}'.format(global_dir, uwsgi_socket))
+    conn.sudo(f'cp {global_dir}/uwsgi-app@.socket {uwsgi_socket}')
     set_permissions_file(conn, uwsgi_socket, 'root', 'root', '644')
 
-    conn.sudo('cp {0}/uwsgi-app@.service {1}'.format(global_dir, uwsgi_service))
+    conn.sudo(f'cp {global_dir}/uwsgi-app@.service {uwsgi_service}')
     set_permissions_file(conn, uwsgi_service, 'root', 'root', '644')
 
     conn.sudo('/etc/init.d/nginx restart')
@@ -260,9 +259,9 @@ def shutdown(conn, config, branch=None, secret_branch=None):
                             config, branch, secret_branch))
 
     repo_dir = get_repo_dir(config)
-    nginx_dir = '{0}/config/ubuntu-22.04/nginx/shutdown'.format(repo_dir)
+    nginx_dir = f'{repo_dir}/config/ubuntu-22.04/nginx/shutdown'
     secret_repo_dir = get_secret_repo_dir(config)
-    ssl_dir = '{0}/{1}/ssl'.format(secret_repo_dir, config)
+    ssl_dir = f'{secret_repo_dir}/{config}/ssl'
 
     _update_source(conn, repo_dir, branch)
     _update_source(conn, secret_repo_dir, secret_branch)
