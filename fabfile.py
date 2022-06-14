845a9cc412c7270b00657d4004704f7fb2b0a2ab
Kevin Barnes <kbarnes3@gmail.com>
2022-06-13 19:01:17 -0700

    Fixing linting

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric_utils.deploy import deploy, deploy_global_config, shutdown
-from fabric_utils.setup import add_authorized_key, disable_ssh_passwords, setup_deployment, setup_server, setup_superuser, setup_user
+from fabric_utils.setup import add_authorized_key, disable_ssh_passwords, setup_deployment
+from fabric_utils.setup import setup_server, setup_superuser, setup_user
 from fabric_utils.update import compile_requirements
 
 __all__ = [
