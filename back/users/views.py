253f7edb44095656e47709fe873fc0170da1afa4
Kevin Barnes <kbarnes3@gmail.com>
2024-06-13 11:41:11 -0700

    Remove unused code

--- back/users/views.py
+++ back/users/views.py
@@ -1,29 +1,4 @@
-from django.contrib.auth import authenticate, login
-from django.core.mail import mail_admins
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
-
-from users.forms import RegistrationForm
-
-
-def create_user_account(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            primary_email = form.cleaned_data['primary_email']
-            password = form.cleaned_data['password1']
-
-            mail_admins('New user account created',
-                f'A new user account was created for {primary_email}')
-
-            user = authenticate(username=primary_email, password=password)
-            login(request, user)
-            return redirect('/')
-    else:
-        form = RegistrationForm()
-
-    return render(request, 'users/create_user_account.html', {'form': form})
 
 
 def logged_in_api(request):
