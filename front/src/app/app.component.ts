1e3c7af6271ca7cfdbd4f5175894832e1955c996
Kevin Barnes <kbarnes3@gmail.com>
2025-01-01 21:18:34 -0800

    Fix ng serve

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { versionInfo } from './version-info';
 import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterModule],
+    imports: [RouterModule, NavBarComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     
