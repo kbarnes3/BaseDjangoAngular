103afd1b6fed2f8ddfd4e4bde848075d2dec2321
Kevin Barnes <kbarnes3@gmail.com>
2025-12-15 18:48:07 -0800

    Update to @angular/core @ "21.0.5"

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
@@ -15,7 +15,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, NgbModule),
+        provideZoneChangeDetection(),importProvidersFrom(BrowserModule, AppRoutingModule, NgbModule),
         provideHttpClient(withInterceptorsFromDi())
     ]
 })
