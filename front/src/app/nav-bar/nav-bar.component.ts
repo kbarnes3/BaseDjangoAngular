360d240e60fbfd93adeb0897a4766bb891826fd2
Kevin Barnes <kbarnes3@gmail.com>
2026-02-11 21:22:01 -0800

    Fix Angular logout

--- front/src/app/nav-bar/nav-bar.component.ts
+++ front/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, DOCUMENT } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {LoginStatus, LoginStatusService} from '../login-status.service';
 
@@ -12,6 +12,7 @@ import {LoginStatus, LoginStatusService} from '../login-status.service';
 })
 export class NavBarComponent implements OnInit {
   private statusService = inject(LoginStatusService);
+  private document = inject(DOCUMENT);
 
   public isCollapsed: boolean;
   public status: LoginStatus;
@@ -23,6 +24,22 @@ export class NavBarComponent implements OnInit {
         .subscribe((status: LoginStatus) => {
           this.status = status;
         });
+  }
+
+  logout(): void {
+    const form = this.document.createElement('form');
+    form.method = 'POST';
+    form.action = '/account/logout/';
+
+    const csrfInput = this.document.createElement('input');
+    csrfInput.type = 'hidden';
+    csrfInput.name = 'csrfmiddlewaretoken';
+    const match = this.document.cookie.match(/csrftoken=([^;]+)/);
+    csrfInput.value = match ? match[1] : '';
+
+    form.appendChild(csrfInput);
+    this.document.body.appendChild(form);
+    form.submit();
   }
 
 }
