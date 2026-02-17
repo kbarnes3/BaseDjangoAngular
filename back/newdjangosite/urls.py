b1d9938c8a92ab1704f0a5f98d1d317dce6f5330
Kevin Barnes <kbarnes3@gmail.com>
2026-02-16 21:31:19 -0800

    Remove Django views and related files

--- back/newdjangosite/urls.py
+++ back/newdjangosite/urls.py
@@ -2,11 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from common.views import hello_world
-
 urlpatterns = [
-    path('', hello_world, name='landing_page'),
-
     path('admin/', admin.site.urls),
 
     path('_allauth/', include('allauth.headless.urls')),
