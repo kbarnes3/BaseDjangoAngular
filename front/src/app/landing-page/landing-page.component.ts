e002003d6c85c1183b7a2952ecd351759046b6b1
Kevin Barnes <kbarnes3@gmail.com>
2026-03-08 19:27:22 -0700

    Fix linting config

--- front/src/app/landing-page/landing-page.component.ts
+++ front/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-landing-page',
@@ -9,7 +9,4 @@ import { Component, OnInit } from '@angular/core';
     
 })
 export class LandingPageComponent {
-
-  constructor() { }
-
 }
